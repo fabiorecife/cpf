1785360d4f21785f2ae3c973a624d7acfdf6b2a9
fabioalmeida <fabiorecife@gmail.com>
2018-09-09 22:18:08 -0300

    generate, format and verificationDigits

--- index.js
+++ index.js
@@ -10,25 +10,53 @@ let CPF = {
     isNumber (numero) {
       return /^\d+$/.test(numero)
     },
-    isValid (_numero) {
-        let first9, last2, digit1, calc10, digit2, numero
-        if ((typeof _numero) !== 'string') {
-            numero = _numero.join('')
-        } else {
-            numero = _numero
-        }
-        numero = this.removePunctuation(numero)
-        if (numero.length !== 11) return false
-        if (!this.isNumber(numero)) return false
-        if (this.allNumberSame(numero)) return false
-
-        first9 = numero.slice(0,9)
-        last2 = parseInt(numero.slice(9,11))
-        digit1 = modulo11.checkDigit(first9)
-        calc10 = first9 + digit1
+    checkDigit(cpfWihoutCheckDigits) {
+        let digit1, digit2, calc10
+        digit1 = modulo11.checkDigit(cpfWihoutCheckDigits)
+        calc10 = cpfWihoutCheckDigits + digit1
         digit2 = modulo11.checkDigit(calc10)
+        return ((digit1*10)+digit2)
+    },
+    verificationDigits (cpfWihoutCheckDigits) {
+      let digit = this.checkDigit(cpfWihoutCheckDigits)
+      digit = digit.toString()
+      if (digit.length < 2) {
+          digit = '0' + digit
+      }
+      return digit
+    },
+    randomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max))
+    },
+    generate() {
+      let digits = []
+      while (digits.length < 9) {
+          digits.push(this.randomInt(9))
+      }
+      digits = digits.join('')
+      return digits+this.verificationDigits(digits)
+    },
+    format(_cpf) {
+        let cpf = this.removePunctuation(_cpf)
+        return cpf.slice(0,3) + '.' + cpf.slice(3,6) + '.' + cpf.slice(6,9) + '-' + cpf.slice(9,11)
+    },
+    isValid (cpf) {
+        let first9, last2, cpfString, digits
+        if ((typeof cpf) !== 'string') {
+            cpfString = cpf.join('')
+        } else {
+            cpfString = cpf
+        }
+        cpfString = this.removePunctuation(cpfString)
+        if (cpfString.length !== 11) return false
+        if (!this.isNumber(cpfString)) return false
+        if (this.allNumberSame(cpfString)) return false
 
-        return last2 === ((digit1*10)+digit2)
+        first9 = cpfString.slice(0,9)
+        last2 = parseInt(cpfString.slice(9,11))
+        digits = this.checkDigit(first9)
+
+        return last2 === digits
     }
 }
 
--- index.test.js
+++ index.test.js
@@ -6,6 +6,8 @@ test('cpf is valid', () => {
     expect(cpf.isValid([8,4,4,3,4,3,3,9,0,6,0])).toBe(true)
     expect(cpf.isValid('04436338002')).toBe(true)
     expect(cpf.isValid('044.363.380-02')).toBe(true)
+    expect(cpf.isValid('30919609007')).toBe(true)
+
 })
 
 test('cpf is invalid', () => {
@@ -13,4 +15,29 @@ test('cpf is invalid', () => {
     expect(cpf.isValid('00000000000')).toBe(false)
     expect(cpf.isValid('11111111111')).toBe(false)
     expect(cpf.isValid('044.363.380-ab')).toBe(false)
+})
+
+test('checkDigit', () => {
+    expect(cpf.checkDigit('021116060')).toBe(14)
+    expect(cpf.checkDigit('111111111')).toBe(11)
+    expect(cpf.checkDigit('000000000')).toBe(0)
+    expect(cpf.checkDigit('309196090')).toBe(7)
+
+})
+
+test('verificationDigits', () => {
+    expect(cpf.verificationDigits('309196090')).toBe('07')
+    expect(cpf.verificationDigits('021116060')).toBe('14')
+})
+
+test('cpf generator', () => {
+    let cpfNumber = cpf.generate()
+    expect(cpf.isValid(cpfNumber)).toBe(true)
+})
+
+test('format cpf', () => {
+    let cpfNumber = cpf.generate()
+    formatedCpf = cpf.format(cpfNumber)
+    console.log(formatedCpf)
+    expect(formatedCpf).toMatch(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)
 })
